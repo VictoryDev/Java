33d61ca04389f86010403ce54da4b552464dcb69
VictoryDev <victoryle1993@gmail.com>
2019-08-21 14:19:37 -0700

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Victory Le");
+        System.out.println("I am 26 years old");
+        System.out.println("I live in San Jose, CA");
+    }
+}
